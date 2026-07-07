5bfd80de06711e21b4530ff6b8f20dbe1ccd5105
pranshu123321 <pranshutiwari19052005@gmail.com>
2026-07-07 17:43:31 +0530

    Configure app providers for Redux, routing, and theme context

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,9 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter } from "react-router";
+import {store} from "./store/store.js"
+import { Provider } from 'react-redux';
+import { ThemeProvider } from './context/ThemeContext';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <ThemeProvider>
+    <Provider store={store}>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
+    </Provider>
+    </ThemeProvider>
   </StrictMode>,
 )
